cfe7de90c5efecdcbb13a9ce032299f1f26cb48d
Yagna Sree <yagnasri1753@gmail.com>
2023-12-20 07:39:06 +0530

    Basic Java Projects

---
+++ src/OOPs/Class_Objects/Main.java
@@ -0,0 +1,10 @@
+package OOPs.Class_Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        //Creating an object for car
+        car mycar = new car("Red", "Toyota");
+        //Calling method for object
+        mycar.displayInfo();
+    }
+}
---
+++ src/OOPs/Class_Objects/Main2.java
@@ -0,0 +1,11 @@
+package OOPs.Class_Objects;
+
+public class Main2 {
+    public static void main(String[] args){
+        // creating object for Area
+        Rectangle Area = new Rectangle(4 , 2);
+
+        //Calling method for object
+        Area.calculateArea();
+    }
+}
