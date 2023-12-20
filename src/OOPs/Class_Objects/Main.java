9f2727f474ac5c8563ec27a41f0357e475752db6
Yagna Sree <yagnasri1753@gmail.com>
2023-12-20 07:16:02 +0530

    Basic Projects

--- src/OOPs/Class_Objects/Main.java
+++
@@ -1,9 +0,0 @@
-package Class_Ojects;
-public class Main {
-    public static void main(String[] args) {
-        //Creating an object for car
-        car mycar = new car("Red", "Toyota");
-        //Calling method for object
-        mycar.displayInfo();
-    }
-}
